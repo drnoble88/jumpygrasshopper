4d21cb967532214510c8db9892a7afc25efacdcb
Dylan <drnoble888@gmail.com>
2023-10-07 15:15:00 -0600

    adjusted grasshopper jumping to be accurate

--- jumpygrasshopper/Assets/Scenes/Grasshopper.cs
+++ jumpygrasshopper/Assets/Scenes/Grasshopper.cs
@@ -11,7 +11,7 @@ public class Grasshopper : MonoBehaviour
     private float currentRotation = 280f; // Track the current rotation angle
     private bool rotateClockwise = true; // Indicates whether to rotate clockwise or counterclockwise
     private float highAngle = 359f;
-    private float lowAngle = 295f;
+    private float lowAngle = 275f;
     
 
     private void Awake()
@@ -20,11 +20,17 @@ public class Grasshopper : MonoBehaviour
         rb.isKinematic = true; // Start with kinematic set to true
     }
 
-    private void Update()
+        private void Update()
     {
         if (!hasBeenLaunched)
         {
             RotateGrasshopper();
+        }
+
+        // Check if the grasshopper is out of the screen boundaries
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RespawnGrasshopper();
         }
 
         if (!hasBeenLaunched && Input.GetKeyDown(KeyCode.Space))
@@ -32,6 +38,23 @@ public class Grasshopper : MonoBehaviour
             LaunchGrasshopper();
         }
     }
+
+    void RespawnGrasshopper()
+    {
+        // Reset the grasshopper's position and rotation
+        transform.position = new Vector3(-7.07f, -2.13f, 0f);
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+        // Reset rotation variables
+        currentRotation = 280f;
+        rotateClockwise = true;
+
+        // Reset launch state
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        rb.isKinematic = true;
+        hasBeenLaunched = false;
+    } 
 
     void RotateGrasshopper()
     {
@@ -57,10 +80,53 @@ public class Grasshopper : MonoBehaviour
 
     void LaunchGrasshopper()
     {
-        if (rb != null)
-        {
+        if (currentRotation <= 276f) {
             rb.isKinematic = false; // Disable kinematic to allow physics to take over
-            rb.AddForce(transform.up * launchForce, ForceMode2D.Impulse); // Launch upwards in 2D space
+            rb.AddForce(transform.up * 35f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 277f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 33f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 278f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 31f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 280f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 28f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 283f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 25f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 288f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 22f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 293f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 20f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 300f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 18f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 306f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 16f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 324f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 13.5f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else if (currentRotation <= 342f){
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 12f, ForceMode2D.Impulse); // Launch upwards in 2D space
+            hasBeenLaunched = true; // Mark the object as launched
+        } else {
+            rb.isKinematic = false; // Disable kinematic to allow physics to take over
+            rb.AddForce(transform.up * 15f, ForceMode2D.Impulse); // Launch upwards in 2D space
             hasBeenLaunched = true; // Mark the object as launched
         }
     }
